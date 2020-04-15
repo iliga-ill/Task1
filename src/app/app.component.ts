21105c045df9207018637682b8890c0c5dd3ac45
iliga-ill <illvolkov@mail.ru>
2020-04-15 20:03:13 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,66 +10,29 @@ import { range } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent extends Component {
+export class AppComponent {
   title = 'Task1';
 
   arr = [
-    {"ID":0,"Name":1,"Disabled":false,"Block":false},
-    {"ID":1,"Name":2,"Disabled":false,"Block":false},
-    {"ID":2,"Name":3,"Disabled":false,"Block":false},
-    {"ID":3,"Name":4,"Disabled":false,"Block":false},
-    {"ID":4,"Name":5,"Disabled":false,"Block":false},
-    {"ID":5,"Name":6,"Disabled":false,"Block":false},
-    {"ID":6,"Name":7,"Disabled":false,"Block":false},
-    {"ID":7,"Name":8,"Disabled":false,"Block":false},
-    {"ID":8,"Name":9,"Disabled":false,"Block":false},
-    {"ID":9,"Name":10,"Disabled":false,"Block":false},
+    {"ID":0,"Name":1,"Block":true},
+    {"ID":1,"Name":2,"Block":true},
+    {"ID":2,"Name":3,"Block":true},
+    {"ID":3,"Name":4,"Block":true},
+    {"ID":4,"Name":5,"Block":true},
+    {"ID":5,"Name":6,"Block":true},
+    {"ID":6,"Name":7,"Block":true},
+    {"ID":7,"Name":8,"Block":true},
+    {"ID":8,"Name":9,"Block":true},
+    {"ID":9,"Name":10,"Block":true},
   ];
 
-
-  Build() {
-    let field = parent.document.getElementById("field");
-    this.RandStatus();
-    this.clear(field);
-    for (let i of this.arr) {
-      if (!i.Disabled){
-        let newElm = parent.document.createElement('div')
-        if (i.Status==true){newElm.setAttribute('class', 'card col-sm-12 col-md-6 col-lg-3 bg-success')}
-        else{newElm.setAttribute('class', 'card col-sm-12 col-md-6 col-lg-3 bg-danger w-25')}
-        i["Block"] = newElm;
-        field.append(newElm)
-        //let bodyItem = document.getElementById(${item.id})
-        let bodyItem = document.createElement('div')
-        bodyItem.setAttribute('class', 'card-body')
-        newElm.append(bodyItem)
-        //let headerItem = document.getElementById(b${item.id})
-        let ID = document.createElement('h5')
-        ID.setAttribute("class", 'card-title')
-        ID.innerHTML = `ID:${i.ID}`;
-        bodyItem.append(ID);
-        let Br1 = document.createElement('br');
-        bodyItem.append(Br1);
-        let Name = document.createElement('a')
-        Name.setAttribute("class", 'card-title pl-0 ml-0')
-        Name.innerHTML = `Name:${i.Name}`;
-        bodyItem.append(Name);
-        let Br2 = document.createElement('br');
-        bodyItem.append(Br2);
-        let Status = document.createElement('a')
-        Status.setAttribute("class", 'card-title')
-        Status.innerHTML = `Status:<br>${i.Status}`;
-        bodyItem.append(Status);
-        let Br3 = document.createElement('br');
-        bodyItem.append(Br3);
-        let Button = document.createElement('div')
-        Button.setAttribute("type", 'button')
-        Button.setAttribute("ID", `${i.ID}`)
-        Button.setAttribute("class", 'btn btn-secondary')
-        //Button.setAttribute("(click)", "removeD(event)")
-        Button.innerHTML = `off`;
-        bodyItem.append(Button);
-    }}
-  };
+  constructor(){
+    if (this.arr[0]["Status"]==null){
+      for (let i of this.arr) {
+        i["Status"]=Math.round(Math.random())==1;
+      }
+    }
+  }
 
   addD(event){
     let Id = parent.document.getElementById("input1");
@@ -85,39 +48,35 @@ export class AppComponent extends Component {
     console.log(ID);
     if (ID!=false){
       this.arr[ID].Name=Name.value;
-      this.arr[ID].Disabled=false;
+      this.arr[ID].Block=true;
       console.log(this.arr[ID].Name);
       console.log(this.arr[ID].Block);
     }
     else{
-      this.arr.push({"ID":Id.value,"Name":Name.value,"Disabled":false,"Block":false,"Status":(Math.round(Math.random())==1)});
+      this.arr.push({"ID":Id.value,"Name":Name.value,"Block":true,"Status":(Math.round(Math.random())==1)});
     }
   }
 
   removeD(event){
-    let field = parent.document.getElementById("field");
-    let id=event.target.id;
-    if (id!=""){
-      this.arr[id].Disabled=true;
-      field.removeChild(this.arr[id].Block);
-      this.arr[id].Block=false;
-    }
-  }
-
-  clear(field){
-      for (let i of this.arr) {
-        if (i.Disabled==false&&i.Block!=false){
-          field.removeChild(i.Block);
+    console.log(event);
+    console.log(event.toElement);
+    for (let i=0;i<this.arr.length;i++) {
+      //console.log(this.arr[i].ID+"=="+event.target.id);
+      if (this.arr[i].ID==event.target.id){
+        this.arr[i].Block=false;
+        let bufer1=this.arr[i]
+        let J;
+        for (let j=i;j<this.arr.length-1;j++) {
+          this.arr[j]=this.arr[j+1];
+          J=j;
         }
-      }
-  }
-
-  RandStatus(){
-    if (this.arr[0]["Status"]==null){
-      for (let i of this.arr) {
-        i["Status"]=Math.round(Math.random())==1;
+        console.log(this.arr[J].ID);
+        this.arr[this.arr.length-1]=bufer1
+        console.log(this.arr);
+        break;
       }
     }
+
   }
 
 }
